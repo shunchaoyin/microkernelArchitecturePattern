e3f081d238c69387b815157bdc81b0cc02269c98
shunchaoyin <shunchaoyin@gmail.com>
2024-11-29 19:11:31 +0800

    Update Core.cpp

--- Core/Core.cpp
+++ Core/Core.cpp
@@ -1,11 +1,14 @@
 #include "Core.h"
+#include <stdexcept>
+#include <windows.h>
 
 Core::Core()
-{}
+{
+}
 
 Core::~Core()
 {
-	for (auto it : _consummers)
+	for (auto it : _consumers)
 	{
 		for (APlugin* plugin : it.second)
 			plugin->unregisterPlugin();
@@ -17,10 +20,10 @@ Core::~Core()
 void Core::loadPlugin(std::string path)
 {
 	HINSTANCE lib;
-	lib = LoadLibrary(std::wstring(path.begin(), path.end()).c_str());
+	lib = LoadLibraryW(std::wstring(path.begin(), path.end()).c_str());
 	if (lib == nullptr)
 	{
-		throw std::runtime_error("");
+		throw std::runtime_error("Failed to load library");
 		return;
 	}
 	_plugins.push_back(lib);
@@ -28,13 +31,13 @@ void Core::loadPlugin(std::string path)
 	PluginDetails* details = (PluginDetails*)GetProcAddress(lib, "exports");
 	if (details == nullptr)
 	{
-		throw std::runtime_error("");
+		throw std::runtime_error("Failed to get plugin details");
 		return;
 	}
 
-	if (details->apiVersion >= PLUGIN_API_VERSION)
+	if (details->apiVersion < PLUGIN_API_VERSION)
 	{
-		throw std::runtime_error("");
+		throw std::runtime_error("Incompatible plugin API version");
 		return;
 	}
 
@@ -44,5 +47,11 @@ void Core::loadPlugin(std::string path)
 void Core::unloadPlugins()
 {
 	for (HINSTANCE lib : _plugins)
+	{
 		bool freeResult = FreeLibrary(lib);
+		if (!freeResult)
+		{
+			throw std::runtime_error("Failed to free library");
+		}
+	}
 }
